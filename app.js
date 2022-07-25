1fc71af7d6ff4fd58adaafa13a1c60cbb4c1aeb0
IgnacioJC <igncanton.dev@gmail.com>
2022-07-25 12:28:56 -0300

    routes fixed

---
+++ app.js
@@ -0,0 +1,212 @@
+//Elements from the DOM
+const itemList = document.getElementById('itemList')
+const inputId = document.getElementById('inputID')
+const inputProduct = document.getElementById('inputProduct')
+const inputAmount = document.getElementById('inputAmount')
+const inputBtn = document.getElementById('inputBtn')
+const emptyBtn = document.getElementById('emptyBtn')
+const logOutBtn = document.getElementById('logOutBtn')
+
+
+//Main array containing all the products added in local storage
+let itemsArray = []
+
+//Svg icons
+const editIcon = `<img src="./img/editIcon2.svg" style="width:16px; pointer-events: none" alt="Edit trash icon">`
+const trashIcon = `<img src="./img/deleteIcon2.svg" style="width:16px; pointer-events: none" alt="Delete trash icon">`
+
+//Creates all the items maping the main array
+const itemsCreation = () => {
+    itemsArray.forEach(itemMap => {
+        let newItem = document.createElement('div')
+        newItem.classList.add('itemContainer')
+        newItem.id = `${itemMap.id}`
+        newItem.innerHTML = `
+                <p class="itemId" id="itemId">${itemMap.id}</p>
+                <p class="itemName" id="itemName">${itemMap.item}</p>
+                <p class="itemAmount" id="itemAmount">${itemMap.stock}</p>
+                <button class="btn btnEdit" id="btnEdit">${editIcon}</button>
+                <button class="btn btnDelete" id="btnDelete">${trashIcon}</button>
+        `
+        itemList.appendChild(newItem)
+    });
+}
+
+//Checks if there is a localStorage created, otherwise it creates one
+const localStorageChecker = () => {
+    if (localStorage.getItem('stockApp')) {
+        itemsArray = JSON.parse(localStorage.getItem('stockApp'))
+        itemsCreation()
+    } else {
+        localStorage.setItem('stockApp', itemsArray)
+        itemsCreation()
+    }
+}
+
+localStorageChecker()
+
+//Input event listener
+inputBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let idProduct = inputId.value
+    let product = inputProduct.value
+    let amount = inputAmount.value
+
+    if (idProduct === '' || amount === '' || product === '') {
+
+        alert("Campos vacios: ingrese ID, Nombre y Cantidad del producto")
+
+    } else {
+
+        let itemObject = { id: idProduct, item: product, stock: amount }
+
+        itemsArray.push(itemObject)
+
+        let item = document.createElement('div')
+        item.classList.add('itemContainer')
+        item.setAttribute('id', `${idProduct}`)
+        item.innerHTML = `
+                <p class="itemId" id="itemId">${idProduct}</p>
+                <p class="itemName" id="itemName">${product}</p>
+                <p class="itemAmount" id="itemAmount">${amount}</p>
+                <button class="btn btnEdit" id="btnEdit">${editIcon}</button>
+                <button class="btn btnDelete" id="btnDelete">${trashIcon}</button>
+        `
+
+        itemList.appendChild(item)
+
+        localStorage.setItem('stockApp', JSON.stringify(itemsArray))
+
+        inputId.value = ''
+        inputProduct.value = ''
+        inputAmount.value = ''
+    }
+
+})
+
+//Empties the dom and the localStorage
+emptyBtn.addEventListener('click', () => {
+    localStorage.removeItem('stockApp')
+    itemsArray = []
+    while (itemList.firstChild) {
+        itemList.firstChild.remove()
+    }
+})
+
+//Deletes individual objects
+itemList.addEventListener('click', (e) => {
+    let selectedBtn = e.target.parentElement
+    let parentId = e.target.parentElement.id
+
+    if (e.target.classList.contains('btnDelete')) {
+        itemList.removeChild(selectedBtn)
+
+        let itemFind = itemsArray.findIndex(x => x.id === parentId)
+
+        itemsArray.splice(itemFind, 1)
+
+        localStorage.setItem('stockApp', JSON.stringify(itemsArray))
+    }
+
+    //Edit Functions
+    if (e.target.classList.contains('btnEdit')) {
+        localStorage.setItem('stockApp', JSON.stringify(itemsArray))
+
+        let btnParentId = e.target.parentElement.id
+        let itemsArraySearch = itemsArray.find(x => x.id === btnParentId)
+
+        const originalIdValue = itemsArraySearch.id
+        const originalNameValue = itemsArraySearch.item
+        const originalAmountValue = itemsArraySearch.stock
+
+        //Creates form
+        const newFormEdit = document.createElement('form')
+        newFormEdit.classList.add('formEdit')
+        newFormEdit.id = btnParentId
+        e.target.parentElement.removeChild(e.target.parentElement.childNodes[1])
+
+        e.target.parentElement.childNodes[1].replaceWith(newFormEdit)
+
+        e.target.parentElement.removeChild(e.target.parentElement.childNodes[2])
+        e.target.parentElement.removeChild(e.target.parentElement.childNodes[3])
+        e.target.parentElement.removeChild(e.target.parentElement.childNodes[4])
+
+        //Creates and edits the ID input
+        const newIdInput = document.createElement('input')
+        newIdInput.classList.add('itemId')
+        newIdInput.placeholder = `${originalIdValue}...`
+        newFormEdit.append(newIdInput)
+
+        //Creates and edits the NAME input
+        const newNameInput = document.createElement('input')
+        newNameInput.classList.add('itemName')
+        newNameInput.placeholder = `${originalNameValue}...`
+        newFormEdit.append(newNameInput)
+
+        //Creates and edits the AMOUNT input
+        const newAmountInput = document.createElement('input')
+        newAmountInput.classList.add('itemAmount')
+        newAmountInput.placeholder = `${originalAmountValue}...`
+        newFormEdit.append(newAmountInput)
+
+        //Replaces the edit btn form a submit btn
+        const newSubmitBtn = document.createElement('input')
+        newSubmitBtn.classList.add('btn')
+        newSubmitBtn.classList.add('submitBtn')
+        newSubmitBtn.id = btnParentId
+        newSubmitBtn.type = 'submit'
+        newSubmitBtn.value = '+'
+        newFormEdit.append(newSubmitBtn)
+
+
+        document.getElementById(btnParentId).addEventListener('submit', (e) => {
+            e.preventDefault()
+
+            let newIdValue = newIdInput.value
+            let newNameValue = newNameInput.value
+            let newAmountValue = newAmountInput.value
+
+            const arraySearch = itemsArray.find(x => x.id === e.target.id)
+
+            const emptyChecker = () => {
+                if (newIdValue === '') {
+                    newIdValue = arraySearch.id
+                }
+
+                if (newNameValue === '') {
+                    newNameValue = arraySearch.item
+                }
+
+                if (newAmountValue === '') {
+                    newAmountValue = arraySearch.stock
+                }
+            }
+
+            emptyChecker()
+
+            arraySearch.id = newIdValue
+            arraySearch.item = newNameValue
+            arraySearch.stock = newAmountValue
+            e.target.id = newIdValue
+            selectedBtn.id = newIdValue
+
+            selectedBtn.innerHTML = `
+                <p class="itemId" id="itemId">${newIdValue}</p>
+                <p class="itemName" id="itemName">${newNameValue}</p>
+                <p class="itemAmount" id="itemAmount">${newAmountValue}</p>
+                <button class="btn btnEdit" id="btnEdit">${editIcon}</button>
+                <button class="btn btnDelete" id="btnDelete">${trashIcon}</button>
+        `
+
+            localStorage.setItem('stockApp', JSON.stringify(itemsArray))
+        })
+
+
+    }
+})
+
+logOutBtn.addEventListener('click', () => {
+    window.location.replace("index.html");
+})
+
--- index.js
+++ index.js
@@ -1,212 +1,128 @@
 //Elements from the DOM
-const itemList = document.getElementById('itemList')
-const inputId = document.getElementById('inputID')
-const inputProduct = document.getElementById('inputProduct')
-const inputAmount = document.getElementById('inputAmount')
-const inputBtn = document.getElementById('inputBtn')
-const emptyBtn = document.getElementById('emptyBtn')
-const logOutBtn = document.getElementById('logOutBtn')
+const loginContainer = document.getElementById('loginContainer')
+const loginForm = document.getElementById('loginForm')
+const loginName = document.getElementById('loginName')
+const loginPassword = document.getElementById('loginPassword')
+const btnLogin = document.getElementById('btnLogin')
+const btnCreateAccount = document.getElementById('btnCreateAccount')
 
+//Main users array
+let usersDB = []
 
-//Main array containing all the products added in local storage
-let itemsArray = []
-
-//Svg icons
-const editIcon = `<img src="./img/editIcon2.svg" style="width:16px; pointer-events: none" alt="Edit trash icon">`
-const trashIcon = `<img src="./img/deleteIcon2.svg" style="width:16px; pointer-events: none" alt="Delete trash icon">`
-
-//Creates all the items maping the main array
-const itemsCreation = () => {
-    itemsArray.forEach(itemMap => {
-        let newItem = document.createElement('div')
-        newItem.classList.add('itemContainer')
-        newItem.id = `${itemMap.id}`
-        newItem.innerHTML = `
-                <p class="itemId" id="itemId">${itemMap.id}</p>
-                <p class="itemName" id="itemName">${itemMap.item}</p>
-                <p class="itemAmount" id="itemAmount">${itemMap.stock}</p>
-                <button class="btn btnEdit" id="btnEdit">${editIcon}</button>
-                <button class="btn btnDelete" id="btnDelete">${trashIcon}</button>
-        `
-        itemList.appendChild(newItem)
-    });
-}
-
-//Checks if there is a localStorage created, otherwise it creates one
-const localStorageChecker = () => {
-    if (localStorage.getItem('stockApp')) {
-        itemsArray = JSON.parse(localStorage.getItem('stockApp'))
-        itemsCreation()
+//Database existence checker
+const usersDataBaseChecker = () => {
+    if (localStorage.getItem('StockifyDB')) {
+        usersDB = JSON.parse(localStorage.getItem('StockifyDB'))
     } else {
-        localStorage.setItem('stockApp', itemsArray)
-        itemsCreation()
+        usersDB.push({ user: 'admin', pass: 'admin' })
+        localStorage.setItem('StockifyDB', JSON.stringify(usersDB))
     }
 }
 
-localStorageChecker()
+usersDataBaseChecker()
 
-//Input event listener
-inputBtn.addEventListener('click', (e) => {
+//Login form
+loginForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    let idProduct = inputId.value
-    let product = inputProduct.value
-    let amount = inputAmount.value
+    const inputName = loginName.value
+    const inputPassword = loginPassword.value
 
-    if (idProduct === '' || amount === '' || product === '') {
+    const findArrayName = () => {
+        const nameResult = usersDB.find(x => x.user === inputName)
+        const passResult = usersDB.find(x => x.pass === inputPassword)
 
-        alert("Campos vacios: ingrese ID, Nombre y Cantidad del producto")
-
-    } else {
-
-        let itemObject = { id: idProduct, item: product, stock: amount }
-
-        itemsArray.push(itemObject)
-
-        let item = document.createElement('div')
-        item.classList.add('itemContainer')
-        item.setAttribute('id', `${idProduct}`)
-        item.innerHTML = `
-                <p class="itemId" id="itemId">${idProduct}</p>
-                <p class="itemName" id="itemName">${product}</p>
-                <p class="itemAmount" id="itemAmount">${amount}</p>
-                <button class="btn btnEdit" id="btnEdit">${editIcon}</button>
-                <button class="btn btnDelete" id="btnDelete">${trashIcon}</button>
-        `
-
-        itemList.appendChild(item)
-
-        localStorage.setItem('stockApp', JSON.stringify(itemsArray))
-
-        inputId.value = ''
-        inputProduct.value = ''
-        inputAmount.value = ''
+        if (nameResult === undefined || passResult === undefined) {
+            alert("nombre o contraseña no validos")
+            loginName.value = ''
+            loginPassword.value = ''
+            loginName.focus()
+        } else {
+            window.location.replace("app.html");
+        }
     }
+
+    findArrayName()
+
 
 })
 
-//Empties the dom and the localStorage
-emptyBtn.addEventListener('click', () => {
-    localStorage.removeItem('stockApp')
-    itemsArray = []
-    while (itemList.firstChild) {
-        itemList.firstChild.remove()
-    }
-})
+//Handles all the information and events in the new user form
+const bringNewUserFormElements = () => {
+    const createForm = document.getElementById('createForm')
+    const createNewUserBtn = document.getElementById('createNewUserBtn')
+    const newUserName = document.getElementById('newUserName')
+    const newPassword01 = document.getElementById('newPassword01')
+    const newPassword02 = document.getElementById('newPassword02')
 
-//Deletes individual objects
-itemList.addEventListener('click', (e) => {
-    let selectedBtn = e.target.parentElement
-    let parentId = e.target.parentElement.id
+    createNewUserBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        const userValue = newUserName.value
+        const pass01Value = newPassword01.value
+        const pass02Value = newPassword02.value
 
-    if (e.target.classList.contains('btnDelete')) {
-        itemList.removeChild(selectedBtn)
+        console.log(userValue)
+        console.log(pass01Value)
+        console.log(pass02Value)
 
-        let itemFind = itemsArray.findIndex(x => x.id === parentId)
+        if (userValue === '' || pass01Value === '' || pass02Value === '') {
+            alert("Campos vacios, completelos con sus datos por favor.")
+            newUserName.focus()
+        } else {
+            const newUserSearch = usersDB.find(x => x.user === userValue)
 
-        itemsArray.splice(itemFind, 1)
-
-        localStorage.setItem('stockApp', JSON.stringify(itemsArray))
-    }
-
-    //Edit Functions
-    if (e.target.classList.contains('btnEdit')) {
-        localStorage.setItem('stockApp', JSON.stringify(itemsArray))
-
-        let btnParentId = e.target.parentElement.id
-        let itemsArraySearch = itemsArray.find(x => x.id === btnParentId)
-
-        const originalIdValue = itemsArraySearch.id
-        const originalNameValue = itemsArraySearch.item
-        const originalAmountValue = itemsArraySearch.stock
-
-        //Creates form
-        const newFormEdit = document.createElement('form')
-        newFormEdit.classList.add('formEdit')
-        newFormEdit.id = btnParentId
-        e.target.parentElement.removeChild(e.target.parentElement.childNodes[1])
-
-        e.target.parentElement.childNodes[1].replaceWith(newFormEdit)
-
-        e.target.parentElement.removeChild(e.target.parentElement.childNodes[2])
-        e.target.parentElement.removeChild(e.target.parentElement.childNodes[3])
-        e.target.parentElement.removeChild(e.target.parentElement.childNodes[4])
-
-        //Creates and edits the ID input
-        const newIdInput = document.createElement('input')
-        newIdInput.classList.add('itemId')
-        newIdInput.placeholder = `${originalIdValue}...`
-        newFormEdit.append(newIdInput)
-
-        //Creates and edits the NAME input
-        const newNameInput = document.createElement('input')
-        newNameInput.classList.add('itemName')
-        newNameInput.placeholder = `${originalNameValue}...`
-        newFormEdit.append(newNameInput)
-
-        //Creates and edits the AMOUNT input
-        const newAmountInput = document.createElement('input')
-        newAmountInput.classList.add('itemAmount')
-        newAmountInput.placeholder = `${originalAmountValue}...`
-        newFormEdit.append(newAmountInput)
-
-        //Replaces the edit btn form a submit btn
-        const newSubmitBtn = document.createElement('input')
-        newSubmitBtn.classList.add('btn')
-        newSubmitBtn.classList.add('submitBtn')
-        newSubmitBtn.id = btnParentId
-        newSubmitBtn.type = 'submit'
-        newSubmitBtn.value = '+'
-        newFormEdit.append(newSubmitBtn)
-
-
-        document.getElementById(btnParentId).addEventListener('submit', (e) => {
-            e.preventDefault()
-
-            let newIdValue = newIdInput.value
-            let newNameValue = newNameInput.value
-            let newAmountValue = newAmountInput.value
-
-            const arraySearch = itemsArray.find(x => x.id === e.target.id)
-
-            const emptyChecker = () => {
-                if (newIdValue === '') {
-                    newIdValue = arraySearch.id
+            if (newUserSearch === undefined) {
+                if (pass01Value === pass02Value) {
+                    alert('Creación de usuario exitosa')
+                    usersDB.push({ user: userValue, pass: pass01Value })
+                    localStorage.setItem('StockifyDB', JSON.stringify(usersDB))
+                    location.reload()
+                } else {
+                    alert('Passwords are different, please enter the same password twice for confirmation.')
+                    newPassword01.value = ''
+                    newPassword02.value = ''
+                    newPassword01.focus()
                 }
-
-                if (newNameValue === '') {
-                    newNameValue = arraySearch.item
-                }
-
-                if (newAmountValue === '') {
-                    newAmountValue = arraySearch.stock
-                }
+            } else {
+                alert('that user already exists, choose a different name')
+                newUserName.focus()
             }
+        }
+    })
+}
 
-            emptyChecker()
+//Create new user form
+btnCreateAccount.addEventListener('click', () => {
+    //removes login elements
+    document.getElementById('loginSubtitle').remove()
+    loginForm.remove()
+    btnCreateAccount.remove()
 
-            arraySearch.id = newIdValue
-            arraySearch.item = newNameValue
-            arraySearch.stock = newAmountValue
-            e.target.id = newIdValue
-            selectedBtn.id = newIdValue
+    //creates and appends new subtitle
+    const createAccountSubtitle = document.createElement('p')
+    createAccountSubtitle.innerHTML = 'Create a new account'
+    loginContainer.children[0].insertAdjacentElement('afterend', createAccountSubtitle)
 
-            selectedBtn.innerHTML = `
-                <p class="itemId" id="itemId">${newIdValue}</p>
-                <p class="itemName" id="itemName">${newNameValue}</p>
-                <p class="itemAmount" id="itemAmount">${newAmountValue}</p>
-                <button class="btn btnEdit" id="btnEdit">${editIcon}</button>
-                <button class="btn btnDelete" id="btnDelete">${trashIcon}</button>
-        `
+    //creates new form
+    const createNewAccountLogin = document.createElement('form')
+    createNewAccountLogin.classList.add('form', 'createForm')
+    createNewAccountLogin.id = 'createForm'
+    createNewAccountLogin.autocomplete = 'off'
+    createNewAccountLogin.innerHTML = `
+        <input class="inputLogin" id='newUserName' type="text" placeholder="Enter new username...">
+        <input class="inputLogin" id='newPassword01' type="text" placeholder="Password..." style="-webkit-text-security: disc;">
+        <input class="inputLogin" id='newPassword02' type="text" placeholder="Re-enter Password..." style="-webkit-text-security: disc;">
+        <input class="btnCreateAccount" id='createNewUserBtn' type='submit' value='Create New Account'>
+    `
+    loginContainer.append(createNewAccountLogin)
 
-            localStorage.setItem('stockApp', JSON.stringify(itemsArray))
-        })
+    //creates return to login link
+    const returnAnchor = document.createElement('a')
+    returnAnchor.innerHTML = 'Return to Login'
+    returnAnchor.href = 'login.html'
+    returnAnchor.classList.add('returnAnchor')
+    loginContainer.append(returnAnchor)
 
-
-    }
+    //adds all the functionality to the new user form
+    bringNewUserFormElements()
 })
-
-logOutBtn.addEventListener('click', () => {
-    window.location.replace("login.html");
-})
-
